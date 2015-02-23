37051c099ca87e4ee83e127c8e52ab6b5919181f
Erwan Loisant <eloisant@gmail.com>
2015-02-23 15:00:39 +0100

    Reactivate more tests

--- test/io/prismic/api_test.clj
+++ test/io/prismic/api_test.clj
@@ -5,7 +5,7 @@
 (def lbc (get-api "https://lesbonneschoses.prismic.io/api"))
 (def micro (get-api "https://micro.prismic.io/api"))
 (def test-endpoint "https://test.prismic.io/api")
-(def test
+(def test-repo
   (let [token "MC5VMFB1N0FFQUFDNEEyZF82.77-9AFfvv73vv71D77-977-977-977-9Unk2au-_ve-_vU7vv73vv73vv70u77-977-977-977-9bO-_vQbvv70dKwY"]
     (get-api test-endpoint token)))
 
@@ -16,7 +16,7 @@
     (is= (oauth-initiate lbc) "https://lesbonneschoses.prismic.io/auth"))
 
   (testing "get secured api"
-    (is= (oauth-initiate test) "https://test.prismic.io/auth")))
+    (is= (oauth-initiate test-repo) "https://test.prismic.io/auth")))
 
 ;(deftest oauth-exceptions
 ;  (testing "invalid token"
@@ -35,27 +35,27 @@
 
 (deftest search-documents
 
-;  (testing "count blog posts"
-;    (is= (results-size (search lbc :blog nil)) 6))
+  (testing "count blog posts"
+    (is= (results-size (search lbc :blog nil)) 6))
 
   (testing "fulltext search"
     (let [query "[[:d = fulltext(my.job-offer.name, \"Pastry dresser\")]]"
           response (search lbc :everything query)]
       (is= (-> (results response) first .getId) "UlfoxUnM0wkXYXbh")))
 
-;  (testing "fulltext search in a future release"
-;    (let [query "[[:d = fulltext(my.article.title, \"release\")]]"
-;          response (search test "U0VaPwEAADcA2kNS" :everything query)]
-;      (is= (-> (results response) first .getId) "U0VaMQEAADYA2kMz")))
+  (testing "fulltext search in a future release"
+    (let [query "[[:d = fulltext(my.article.title, \"release\")]]"
+          response (search test-repo "VDP8USsAACsAg3jK" :everything query)]
+      (is= (-> (results response) first .getId) "U0VaEwEAADMA2kLw")))
 
-;  (testing "find by id"
-;    (let [id "UkL0gMuvzYUANCpf" doc (get-by-id lbc id)]
-;      (is= (:id doc) id)))
+  (testing "find by id"
+    (let [id "UlfoxUnM0wkXYXbV" doc (get-by-id lbc id)]
+      (is= (.getId doc) id)))
 
-;  (testing "find by id a document in a future release"
-;    (let [id "U0VaMQEAADYA2kMz"
-;          doc (get-by-id test "U0VaPwEAADcA2kNS" id)]
-;      (is= (.getId doc) id)))
+  (testing "find by id a document in a future release"
+    (let [id "U0VaMQEAADYA2kMz"
+          doc (get-by-id test-repo "VDP8USsAACsAg3jK" id)]
+      (is= (.getId doc) id)))
 
   (testing "find by bookmark"
     (let [doc (get-by-bookmark lbc :stores)
@@ -63,10 +63,10 @@
       (is= text "Don't be a stranger!")))
 )
 
-;  (testing "find by bookmark in a future released"
-;    (let [doc (get-by-bookmark test "U0VaPwEAADcA2kNS" :home)
-;          text (-> (get-fragment doc :title) :value first :text)]
-;      (is= text "Released in the future")))
+  (testing "find by bookmark in a future released"
+    (let [doc (get-by-bookmark test-repo "VDP8USsAACsAg3jK" :home)
+          text (-> (get-fragment doc :title) :value first :text)]
+      (is= text "Released in the future")))
 
 (deftest select-fragments
   (let [job (get-by-id lbc "UlfoxUnM0wkXYXbs")
