cb99e440ae3bf9020255b2cdb7465e9d14680ad9
cchantep <chantepie@altern.org>
2015-04-01 11:38:32 +0200

    Minor dependencies update

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.5"]
-                 [io.prismic/java-kit "1.2.2"]
-                 [cheshire "5.3.1"]
-                 [org.clojure/core.match "0.2.1"]
+                 [org.clojure/data.json "0.2.6"]
+                 [io.prismic/java-kit "1.2.3"]
+                 [cheshire "5.4.0"]
+                 [org.clojure/core.match "0.2.2"]
                  [org.clojure/algo.generic "0.1.2"]])
