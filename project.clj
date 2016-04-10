34d85a734211f0857ba672b8138deb4957736906
tbrooke <tom.brooke@gmail.com>
2016-04-10 18:05:18 -0400

    updated dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://prismic.io"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [io.prismic/java-kit "1.2.3"]
-                 [cheshire "5.4.0"]
-                 [org.clojure/core.match "0.2.2"]
+                 [io.prismic/java-kit "1.3.3"]
+                 [cheshire "5.6.0"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/algo.generic "0.1.2"]])
